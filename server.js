ab2d894021da73906fcb7b676b8baa9f476e5ad2
YEHOVAYIRE Moise <moiseyehovayire@gmail.com>
2026-03-17 09:03:41 +0200

    connect database to the server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+import connectDB from "./config/db.js";
+connectDB();
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Task Manager API running"));
