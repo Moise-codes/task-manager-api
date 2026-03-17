b4fda13e24db9570bee10d1e23cfc5fe16c85334
YEHOVAYIRE Moise <moiseyehovayire@gmail.com>
2026-03-17 09:11:27 +0200

    Add board routes to server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
+import boardRoutes from "./routes/boardRoutes.js";
+
 
 dotenv.config();
 
@@ -9,6 +11,7 @@ import connectDB from "./config/db.js";
 connectDB();
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/boards", boardRoutes);
 
 app.get("/", (req, res) => res.send("Task Manager API running"));
 
