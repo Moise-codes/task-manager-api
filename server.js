7a1382b20f0f6e92b7d61912347403b7e9297149
YEHOVAYIRE Moise <moiseyehovayire@gmail.com>
2026-03-17 09:06:17 +0200

    Update server with auth Routes

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+
 dotenv.config();
 
 const app = express();
 import connectDB from "./config/db.js";
 connectDB();
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => res.send("Task Manager API running"));
 
